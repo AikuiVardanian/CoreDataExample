71797f2c3329c15eb1a46f3469499238322c9e43
Aikui Vardanian <anya.vard.nyan@gmail.com>
2022-05-04 11:28:25 +0700

    Initial Commit

---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Aikui Vardanian on 04.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
